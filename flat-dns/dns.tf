054328013f47ef64b3f6aa3cdfecb4937d3cb445
vasanth <kvasanth373@gmail.com>
2022-12-05 20:03:09 +0530

    added tf code for creating the forwading zone, peering zone and the private zone

---
+++ flat-dns/dns.tf
@@ -0,0 +1,52 @@
+ module "onprem-dns-forwarding-zone" {
+  source     = "https://github.com/terraform-google-modules/terraform-google-cloud-dns"
+  for_each = {for entry in var.onprem_dns_entries:  entry.name => entry}
+  name       = each.value.name
+  domain     = each.value.domain
+  labels     = each.value.labels
+  project_id = var.prod_host_project_id
+  type       = "forwarding"
+  private_visibility_config_networks = [var.prod_host_vpc_link]
+  target_name_server_addresses = each.value.target_name_servers
+}
+
+
+module "onprem-dns-peering-zone" {
+  source                             = "https://github.com/terraform-google-modules/terraform-google-cloud-dns"
+  for_each = {for entry in var.dns_entries:  entry.name => entry}
+  project_id                         = var.non_prod_project_id
+  type                               = "peering"
+  name                               = each.value.name
+  domain                             = each.value.domain
+  private_visibility_config_networks = [var.non_prod_vpc_link]
+  target_network                     = var.prod_host_vpc_link
+  labels                             = each.value.labels
+}
+
+module "gcp-dns-private-zone" {
+  source     = "https://github.com/terraform-google-modules/terraform-google-cloud-dns"
+
+ for_each = {for entry in var.gcp_dns_entries:  entry.name => entry}
+
+  project_id = var.prod_host_project_id
+  type       = "private"
+  name       = each.value.name
+  domain     = each.value.domain
+  labels     = each.value.labels
+
+  private_visibility_config_networks = [each.value.private_visibility_config_networks]
+
+  recordsets = each.value.record_sets
+}
+
+module "gcp-dns-peering-zone" {
+  source                             = "https://github.com/terraform-google-modules/terraform-google-cloud-dns"
+  for_each = {for entry in var.gcp_dns_entries:  entry.name => entry}
+  project_id                         = var.non_prod_project_id
+  type                               = "peering"
+  name                               = each.value.name
+  domain                             = each.value.domain
+  private_visibility_config_networks = [var.non_prod_vpc_link]
+  target_network                     = var.prod_host_vpc_link
+  labels                             = each.value.labels
+}
---
+++ flat-dns/variables.tf
@@ -0,0 +1,145 @@
+variable prod_host_project_id {
+    type = string
+    description = "Production host porject id"
+}
+
+variable prod_host_vpc_link {
+    type = string
+    description = "Production host porject vpc self link"
+}
+
+variable non_prod_vpc_link {
+    type = string
+    description = "Non Prod porject vpc self link"
+}
+
+variable "onprem_dns_entries" {
+  type = list(object({
+        name = string
+        domain = string
+        labels = map(any)
+        target_name_servers = list(object({
+            ipv4_address    = string,
+            forwarding_path = string
+        }))
+  }))
+  default = [
+    {
+      domain             = "corp.example.com"
+      name               = "corp"
+      target_name_servers = [
+        {
+          ipv4_address    = "192.168.0.56",
+          forwarding_path = "default"
+        }
+      ],
+    },
+    {
+      domain             = "corp1.example.com"
+      name               = "corp1"
+      target_name_servers = [
+        {
+          ipv4_address    = "192.168.0.57",
+          forwarding_path = "default"
+        }
+      ],
+    },
+  ]
+}
+
+variable "network_self_links" {
+  description = "Self link of the network that will be allowed to query the zone."
+  default     = []
+}
+
+variable "gcp_private_zone_name" {
+  description = "DNS zone name."
+  default     = "foo-local"
+}
+
+variable "gcp_private_zone_domain" {
+  description = "Zone domain."
+  default     = "foo.local."
+}
+
+variable "labels" {
+  type        = map(any)
+  description = "A set of key/value label pairs to assign to this ManagedZone"
+  default = {
+    owner   = "foo"
+    version = "bar"
+  }
+}
+
+variable "record_sets" {
+    type = list(object({
+        name = string
+        type = string
+        ttl = number
+        records = list(string)
+  }))
+  description = "Add the dns record sets to be added to the zone"
+  default = [
+    {
+      name = "ns"
+      type = "A"
+      ttl  = 300
+      records = [
+        "127.0.0.1",
+      ]
+    }
+  ]
+}
+
+variable "private_visibility_config_networks" {
+  description = "Self link of the network that will be allowed to query the zone."
+  default     = []
+}
+
+
+variable "gcp_dns_entries" {
+  type = list(object({
+        name = string
+        domain = string
+        labels = map(any)
+        private_visibility_config_networks = list(string)
+        record_sets = list(object({
+            name = string
+            type = string
+            ttl = number
+            records = list(string)
+        }))
+  }))
+  default = [
+    {
+      domain             = "gcp.corp.example.com"
+      name               = "corp"
+      private_visibility_config_networks = ["exmaple.selflink"]
+      [
+        {
+            name = "ns"
+            type = "A"
+            ttl  = 300
+            records = [
+                "127.0.0.1",
+            ]
+        }
+      ]   
+    },
+    {
+      domain             = "corp1.example.com"
+      name               = "corp1"
+      private_visibility_config_networks = ["exmaple1.selflink"]
+      [
+        {
+            name = "ns"
+            type = "A"
+            ttl  = 300
+            records = [
+                "127.0.0.2",
+            ]
+        }
+      ]
+    },
+  ]
+}
